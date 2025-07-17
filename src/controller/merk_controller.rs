27e0d33616b3757c410d346b0ab4ee327071d57c
willy willy <medicalawn@gmail.com>
2025-07-17 16:45:12 +0700

    [feature] delete merk and post merk

--- src/controller/merk_controller.rs
+++ src/controller/merk_controller.rs
@@ -1,5 +1,5 @@
 use actix_web::{web, HttpResponse, Responder};
-use crate::service::merk_service::MerkService;
+use crate::service::merk_service::{MerkPostDto, MerkService};
 use deadpool_postgres::Pool;
 
 async fn get_merks() -> impl Responder{
@@ -16,11 +16,37 @@ async fn get_merks_3(pool: web::Data<Pool>) -> impl Responder{
     MerkService::say_merk_3(pool.get_ref()).await.unwrap_or_else(|_| HttpResponse::InternalServerError().finish())
 }
 
+async fn post_merks(
+    pool: web::Data<Pool>,
+    data: web::Json<MerkPostDto>
+) -> impl Responder{
+    MerkService::post_merk(pool.get_ref(), data.into_inner())
+        .await
+        .unwrap_or_else(|_| HttpResponse::InternalServerError().finish())
+}
+
+async fn patch_merks() -> impl Responder{
+    HttpResponse::Ok().body("patch merks")
+}
+
+async fn delete_merks(
+    pool: web::Data<Pool>,
+    path: web::Path<String>,
+) -> impl Responder{
+    let id = path.into_inner();
+    MerkService::delete_merk(pool.get_ref(), id)
+        .await
+        .unwrap_or_else(|_| HttpResponse::InternalServerError().finish())
+}
+
 pub fn init_routes(cfg: &mut web::ServiceConfig){
     cfg.service(
         web::scope("/merks")
                     .route("", web::get().to(get_merks))
                     .route("/v2",web::get().to(get_merks_2))
                     .route("/v3",web::get().to(get_merks_3))
+                    .route("", web::post().to(post_merks))
+                    .route("/{id}",web::delete().to(delete_merks))
+                    .route("/{id}",web::patch().to(patch_merks))
     );
 }
--- src/service/merk_service.rs
+++ src/service/merk_service.rs
@@ -1,5 +1,5 @@
-use actix_web::HttpResponse;
-use serde::Serialize;
+use actix_web::{HttpResponse,error};
+use serde::{Serialize, Deserialize};
 use deadpool_postgres::Pool;
 use tokio_postgres::Row;
 
@@ -9,6 +9,14 @@ pub struct MerkService;
 pub struct MerkData{
     c_nm: String,
     b_enabled: i32,
+    c_alias: String,
+}
+
+#[derive(Deserialize)]
+pub struct MerkPostDto {
+    pub c_nm: String,
+    pub b_enabled: i32,
+    pub c_alias: String,
 }
 
 impl MerkService{
@@ -25,13 +33,14 @@ impl MerkService{
             actix_web::error::ErrorInternalServerError(e)
         })?;
 
-        let rows: Vec<Row> = client.query("SELECT c_nm, b_enabled FROM merk_master", &[])
+        let rows: Vec<Row> = client.query("SELECT c_nm, b_enabled, c_alias FROM merk_master", &[])
             .await
             .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
 
         let merk_list: Vec<MerkData> = rows.iter().map(|row| MerkData {
             c_nm: row.get("c_nm"),
             b_enabled: row.get("b_enabled"),
+            c_alias: row.get("c_alias"),
         }).collect();
 
         let json_response = serde_json::to_string(&merk_list)
@@ -40,5 +49,26 @@ impl MerkService{
         Ok(HttpResponse::Ok()
             .content_type("application/json")
             .body(json_response))
+    }
+
+    pub async fn delete_merk(pool: &Pool, id: String) -> Result<HttpResponse, actix_web::Error> {
+        let client = pool.get().await.map_err(error::ErrorInternalServerError)?;
+        let result = client.execute("DELETE FROM merk_master WHERE c_nm = $1", &[&id])
+            .await.map_err(error::ErrorInternalServerError)?;
+        if result == 0 {
+            Ok(HttpResponse::NotFound().body("Merk not found"))
+        } else {
+            Ok(HttpResponse::Ok().body("Merk deleted"))
+        }
+    }
+
+    pub async fn post_merk(pool: &Pool, data: MerkPostDto) -> Result<HttpResponse, actix_web::Error> {
+        let client = pool.get().await.map_err(error::ErrorInternalServerError)?;
+        client.execute(
+            "INSERT INTO merk_master (c_nm, b_enabled, c_alias) VALUES ($1, $2, $3)",
+            &[&data.c_nm, &data.b_enabled, &data.c_alias]
+        ).await.map_err(error::ErrorInternalServerError)?;
+
+        Ok(HttpResponse::Created().body("Merk created"))
     }
 }
